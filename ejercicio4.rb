467119e1720b895cbac9e867f3733e8dcb67ef2e
Tomas <oyarzun.tomas@gmail.com>
2018-07-23 21:19:26 -0400

    Started menu

--- ejercicio4.rb
+++ ejercicio4.rb
@@ -1,4 +1,11 @@
 option = 0
-while option != 4
-
+while option != 6
+	puts 'Ingrese 1 para conocer la cantidad de productos existentes'
+	puts 'Ingrese 2 para conocer la cantidad de stock total'
+	puts 'Ingrese 3 para conocer los productos no registrados en bodega'
+	puts 'Ingrese 4 para conocer el stock de productos menor al valor ingresado'
+	puts 'Ingrese 5 para registrar un nuevo producto con su stock'
+	puts 'Ingrese 6 para salir'
+	option = gets.chomp.to_i
 end
+
