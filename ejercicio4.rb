919b9a93826d065aa8d286e7e78dd3a5f4188638
Tomas <oyarzun.tomas@gmail.com>
2018-07-24 01:29:44 -0400

    ADD: Methods for sub_menu

--- ejercicio4.rb
+++ ejercicio4.rb
@@ -1,15 +1,43 @@
-#Reading info.txt
-file = File.open('info.txt', 'r')
-products = file.readlines
-file.close
-
-def product_exist(file)
-	a = file.map{ |elem| elem.split(',') }
-	a.each{ |v| puts v.first }
+#Methods
+def cls
+	system "clear"
 end
 
+def product_exist
+	#Reading info.txt
+	file = File.open('info.txt', 'r')
+	products = file.readlines.map(&:chomp)
+	file.close
+
+	products.each do |product|
+	 	puts product.split(', ')[0]
+	end
+end
+
+def total_stock_store
+	File.open('info.txt', 'r'){ |file| puts file.read}
+end
+
+def total_stock
+	#Reading info.txt
+	file = File.open('info.txt', 'r')
+	products = file.readlines.map(&:chomp)
+	file.close
+
+	products.each do |product|
+	 	a = product.split(', ')[1..3]
+	 	b = a.inject(0){|acum,e| acum + e.to_i }
+	 	puts "#{product.split(', ')[0]} tiene un total de: #{b}"
+	end
+end
+
+
+
+#Menus
 def main_menu
+	option = 0
 	while option != 6 
+		puts '--------------------------------------'
 		puts 'Ingrese 1 para conocer la cantidad de productos existentes'
 		puts 'Ingrese 2 para conocer la cantidad de stock total'
 		puts 'Ingrese 3 para conocer los productos no registrados en bodega'
@@ -17,6 +45,7 @@ def main_menu
 		puts 'Ingrese 5 para registrar un nuevo producto con su stock'
 		puts 'Ingrese 6 para salir'
 		option = gets.chomp.to_i
+		cls
 		case option
 		when 1
 			sub_menu
@@ -37,29 +66,31 @@ def main_menu
 end
 
 def sub_menu
-	puts 'a) Mostrar la existencia por productos.'
-	puts 'b) Mostrar la existencia total por tienda.'
-	puts 'c) Mostrar la existencia total en todas las tiendas.'
-	puts 'd) Volver al menú principal.'
-	option = gets.chomp
-	case option
-		when 'a'
-			product_exist(products)
-		when 'b'
-			total_stock(products)
-		when 'c'
-			total_stock_store
-		when 'd'
-			main_menu
-		else
-			puts 'Ingrese una opción valida'
-			sub_menu
+	option = 'a'
+	while option.downcase != 'd'
+		puts '--------------------------------------'
+		puts 'a) Mostrar la existencia por productos.'
+		puts 'b) Mostrar la existencia total por tienda.'
+		puts 'c) Mostrar la existencia total en todas las tiendas.'
+		puts 'd) Volver al menú principal.'
+		option = gets.chomp
+		cls
+		case option
+			when 'a'
+				product_exist
+			when 'b'
+				total_stock_store
+			when 'c'
+				total_stock
+			when 'd'
+				break
+			else
+				puts 'Ingrese una opción valida'
 		end
+	end
 end
 
-
-
-	
-
+#Start
+main_menu
 
 
